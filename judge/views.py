1734a62d6461694ff1545d92a7fcde1a5d58b692
Sahan-11 <sahandarisi@gmail.com>
2022-07-04 00:13:19 +0530

    Added Views

--- judge/views.py
+++ judge/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from .models import Problem,Solution,TestCase
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the judge index.")
+    problem_list = Problem.objects.all
+    context = {'problem_list': problem_list}
+    return render(request, 'judge/index.html', context)
+
+def detail(request, problem_id):
+    problem = get_object_or_404(Problem, pk=problem_id)
+    return render(request, 'judge/detail.html', {'problem': problem})
